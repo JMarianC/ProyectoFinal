ccc9b8989497b39a3071c46aaf73b0eb5fb04a1a
JMarianC <104396801+JMarianC@users.noreply.github.com>
2022-07-12 11:53:22 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,61 @@
+//EFECTO SCROLL PARA LA BARRA HEADER//
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("abajo", window.scrollY>600);
+})
+
+//DARK MODE//
+
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click' , () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+})
+
+
+//CARRUSEL//
+let slider = document.querySelector(".slider-contenedor")
+let sliderIndividual = document.querySelectorAll(".contenido-slider")
+let contador = 1;
+let width = sliderIndividual[0].clientWidth;
+let intervalo = 3000;
+
+window.addEventListener("resize", function(){
+    width = sliderIndividual[0].clientWidth;
+})
+
+setInterval(function(){
+    slides();
+},intervalo);
+
+
+
+function slides(){
+    slider.style.transform = "translate("+(-width*contador)+"px)";
+    slider.style.transition = "transform .8s";
+    contador++;
+
+    if(contador == sliderIndividual.length){
+        setTimeout(function(){
+            slider.style.transform = "translate(0px)";
+            slider.style.transition = "transform 0s";
+            contador=1;
+        },1500)
+    }
+}
+
+const openModal = document.querySelector('.hero__cta');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal__close');
+
+openModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.add('modal--show');
+});
+
+closeModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.remove('modal--show');
+});
